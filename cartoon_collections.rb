7fe81328a2931ce2b9690462417f1cc5742a844c
Aimee Liang <aimeeeliang@gmail.com>
2020-08-12 19:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, index|
-    vector = index +
+    vector = #{index + 1}
     print "Vector. element"
   end
   vector += 1
